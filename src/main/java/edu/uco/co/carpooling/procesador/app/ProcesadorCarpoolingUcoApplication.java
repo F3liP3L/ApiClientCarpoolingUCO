5356f8018a0230da118189c6c376010abb0a3c2f
juan.cardona <felipe7u7432@gmail.com>
2023-07-31 20:40:01 -0500

    start proyect

---
+++ src/main/java/edu/uco/co/carpooling/procesador/app/ProcesadorCarpoolingUcoApplication.java
@@ -0,0 +1,13 @@
+package edu.uco.co.carpooling.procesador.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesadorCarpoolingUcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesadorCarpoolingUcoApplication.class, args);
+	}
+
+}
