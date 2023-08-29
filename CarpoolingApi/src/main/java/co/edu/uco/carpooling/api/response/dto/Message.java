40a05ac5918670e2735c77ed66ef459dd14949d3
juan.cardona <felipe7u7432@gmail.com>
2023-08-28 20:38:53 -0500

    Cambios en Message de response en la capa de Api.

--- CarpoolingApi/src/main/java/co/edu/uco/carpooling/api/response/dto/Message.java
+++ CarpoolingApi/src/main/java/co/edu/uco/carpooling/api/response/dto/Message.java
@@ -15,6 +15,34 @@ public class Message {
         setLevel(level);
     }
 
+
+    public static Message createMessage(String text, String title, MessageLevel level) {
+        return new Message(text, title, level);
+    }
+
+    public static Message createMessage(String text, MessageLevel level) {
+        return new Message(text, text, level);
+    }
+
+    public static Message createErrorMessage(String text, String title) {
+        return new Message(text, title, MessageLevel.ERROR);
+    }
+
+    public static Message createWarningMessage(String text, String title) {
+        return new Message(text, title, MessageLevel.WARNING);
+    }
+
+    public static Message createInfoMessage(String text, String title) {
+        return new Message(text, title, MessageLevel.INFORMATION);
+    }
+
+    public static Message createSuccessMessage(String text, String title) {
+        return new Message(text, title, MessageLevel.SUCCESS);
+    }
+    public static Message createFatalMessage(String text, String title) {
+        return new Message(text, title, MessageLevel.FATAL);
+    }
+
     public String getText() {
         return text;
     }
