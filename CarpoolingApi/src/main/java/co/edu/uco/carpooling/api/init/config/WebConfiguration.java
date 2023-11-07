f03a5a7df59dc6410b80a1e883a8666159df9ce0
juan.cardona <felipe7u7432@gmail.com>
2023-11-06 21:57:49 -0500

    #task-cors-configuration: Agrega clase de configuración para el CORS 🚀.

---
+++ CarpoolingApi/src/main/java/co/edu/uco/carpooling/api/init/config/WebConfiguration.java
@@ -0,0 +1,19 @@
+package co.edu.uco.carpooling.api.init.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/carpooling/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name());
+    }
+}
