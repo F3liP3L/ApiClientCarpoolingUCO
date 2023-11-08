c8443f4cc0728812422570a00ac12f466f1b6d19
juan.cardona <felipe7u7432@gmail.com>
2023-11-08 01:06:04 -0500

    #Task-Authorized-Category: Agrega puerto de repository para el AuthorizedCategoryEntity.

--- CarpoolingInfrastructure/src/main/java/co/edu/uco/carpooling/infrastructure/adapter/repository/postgressql/impl/DriverPerVehicleRepositoryAdapter.java
+++ CarpoolingInfrastructure/src/main/java/co/edu/uco/carpooling/infrastructure/adapter/repository/postgressql/impl/DriverPerVehicleRepositoryAdapter.java
@@ -6,7 +6,6 @@ import co.edu.uco.carpooling.service.port.repository.DriverPerVehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
